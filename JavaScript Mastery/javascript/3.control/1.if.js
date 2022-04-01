62cfd3db379b7f8205c3411125dbc77e6717b98a
pyeonne <pyeonne@gmail.com>
2022-04-01 21:51:59 +0900

    [📚 Docs(JavsScript Mastery)]: 제어문

---
+++ JavaScript Mastery/javascript/3.control/1.if.js
@@ -0,0 +1,16 @@
+// 조건문 Conditional Statement
+// if(조건) { }
+// if(조건) { } else { }
+// if(조건) { } else if(조건2) { } else { }
+let fruit = 'orange';
+if (fruit === 'apple') {
+    console.log('🍎');
+} else if (fruit === 'orange') {
+    console.log('🍊');
+} else {
+    console.log('😍');
+}
+
+if (2 < 1) {
+    console.log('출력되면 안됨!');
+}
